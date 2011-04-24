e1a787aa27f3b25207227c07cfba61ee129fb724
Themaister <maister@archlinux.us>
2011-04-24 15:06:08 +0200

    dma stuff.

---
+++ cpu/dma.h
@@ -0,0 +1,62 @@
+#ifndef __CPU_DMA_H
+#define __CPU_DMA_H
+
+#include <stdint.h>
+#include "state.h"
+#include "macros.h"
+#include "bus.h"
+#include "memory.h"
+
+// TODO: Optimize to branchless? :D This should be quite slow :v
+static inline void run_dma_channel(unsigned channel)
+{
+   struct dma_channel *chan = &STATUS.dma_channels[channel];
+   static const uint8_t transfer_addr[8][4] = {
+      {0, 0, 0, 0},
+      {0, 1, 0, 1},
+      {0, 0, 0, 0},
+      {0, 0, 1, 1},
+      {0, 1, 2, 3},
+      {0, 1, 0, 1},
+      {0, 0, 0, 0},
+      {0, 0, 1, 1}
+   };
+
+   ssnes_bus_write_2000(0x2100 | (chan->dest + transfer_addr[chan->ctrl & 7][chan->trans_cnt++]), cpu_readl(chan->src.l));
+
+   if (!(chan->ctrl & 0x08))
+   {
+      if (chan->ctrl & 0x10)
+         chan->src.l++;
+      else
+         chan->src.l--;
+   }
+
+   // Check if channel is done.
+   if (--chan->size.w == 0)
+      STATUS.dma_enable &= ~(1 << channel);
+}
+
+static inline void cpu_run_dma(void)
+{
+
+   // Horrible stuff ;)
+   if (STATUS.dma_enable & 0x1)
+      run_dma_channel(0);
+   else if (STATUS.dma_enable & 0x2)
+      run_dma_channel(1);
+   else if (STATUS.dma_enable & 0x4)
+      run_dma_channel(2);
+   else if (STATUS.dma_enable & 0x8)
+      run_dma_channel(3);
+   else if (STATUS.dma_enable & 0x10)
+      run_dma_channel(4);
+   else if (STATUS.dma_enable & 0x20)
+      run_dma_channel(5);
+   else if (STATUS.dma_enable & 0x40)
+      run_dma_channel(6);
+   else if (STATUS.dma_enable & 0x80)
+      run_dma_channel(7);
+}
+
+#endif
